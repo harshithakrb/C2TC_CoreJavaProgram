5abf545eea5296505ef3895322692a7e2c2515e9
harshithakrb <harshitha.krb@gmail.com>
2025-07-21 19:51:37 +0530

    This is an addition java program

--- C2TC_CoreJavaProgram/src/Day1/basics/AdditionDemo.java
+++ C2TC_CoreJavaProgram/src/Day1/basics/AdditionDemo.java
@@ -1,5 +1,17 @@
 package Day1.basics;
+import java.util.Scanner;
 
 public class AdditionDemo {
+
+	public static void main(String[] args) {
+		int a,b,c;
+	    Scanner s=new Scanner(System.in);
+	    System.out.println("Enter the values:");
+	    a=s.nextInt();
+	    b=s.nextInt();
+	    c=a+b;
+		System.out.println("Addition of a="+a+" and b="+b+" is "+c);
+
+	}
 
 }
