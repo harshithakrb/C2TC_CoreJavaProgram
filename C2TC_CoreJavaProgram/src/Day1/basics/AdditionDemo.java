8ab4a390770a385d6c067f91a6e4afccef0793ba
harshithakrb <harshitha.krb@gmail.com>
2025-07-21 20:48:12 +0530

    scanner fn

--- C2TC_CoreJavaProgram/src/Day1/basics/AdditionDemo.java
+++ C2TC_CoreJavaProgram/src/Day1/basics/AdditionDemo.java
@@ -5,7 +5,7 @@ public class AdditionDemo {
 
 	public static void main(String[] args) {
 		int a,b,c;
-	    Scanner s=new Scanner(System.in);
+	    Scanner s = new Scanner(System.in);
 	    System.out.println("Enter the values:");
 	    a=s.nextInt();
 	    b=s.nextInt();
