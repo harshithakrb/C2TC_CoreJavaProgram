443d64ef1e17cb9c3ab1ea02ab81ae24b3234af2
harshithakrb <harshitha.krb@gmail.com>
2025-07-21 20:21:49 +0530

    Java Sample code

---
+++ C2TC_CoreJavaProgram/src/Day1/basics/Sample.java
@@ -0,0 +1,11 @@
+package Day1.basics;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+		System.out.println("This is C2TC CoreJava Training");
+
+	}
+
+}
