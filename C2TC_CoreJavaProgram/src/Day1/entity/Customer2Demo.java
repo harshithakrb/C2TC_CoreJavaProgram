54c9d0ee152d95f0260494950b19bb5c4d97562f
harshithakrb <harshitha.krb@gmail.com>
2025-07-21 21:11:29 +0530

    this java code prints customer details using toString() method.

---
+++ C2TC_CoreJavaProgram/src/Day1/entity/Customer2Demo.java
@@ -0,0 +1,20 @@
+package Day1.entity;
+
+public class Customer2Demo {
+	public static void main(String[] args) {
+		Customer2 c1= new Customer2();
+		c1.setCid(101);
+		c1.setName("Harshii");
+		c1.setCity("Puducherry");
+		System.out.println(c1);
+	
+		
+		Customer2 c2= new Customer2();
+		c2.setCid(101);
+		c2.setName("Bannu");
+		c2.setCity("Chennai");
+		System.out.println(c2);
+		
+
+	}
+}
