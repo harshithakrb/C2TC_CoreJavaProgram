28942534aac5e05c5a8c8aa0e8b4eb1b16262b24
harshithakrb <harshitha.krb@gmail.com>
2025-07-21 20:15:43 +0530

    declaration format of Java method signature

---
+++ C2TC_CoreJavaProgram/src/Day1/basics/MethodsStatic.java
@@ -0,0 +1,26 @@
+package Day1.basics;
+
+public class MethodsStatic 
+{
+	public static void main()//wrong declaration format
+	{
+		System.out.println("Welcome to main 1");
+		
+	}
+	
+	public static void main(char c[])//wrong declaration format
+	{
+		System.out.println("Welcome to main 2");
+	}
+	public static void main(String ar)//wrong declaration format
+	{
+		System.out.println("Welcome to main 3");
+		
+	}
+	public static void main(String[] args) //correct declaration format
+	{
+		System.out.println("Welcome to jfs");
+		System.out.println("Next line");
+	}
+
+}
