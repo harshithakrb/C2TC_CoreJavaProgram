5963dc02458fa30043c7756592bdb72b2ca6b822
harshithakrb <harshitha.krb@gmail.com>
2025-07-22 15:41:36 +0530

    This project contains all core java programs.

--- C2TC_CoreJavaProgram/src/Day2/Constructor/Customer.java
+++ C2TC_CoreJavaProgram/src/Day2/Constructor/Customer.java
@@ -4,11 +4,22 @@ public class Customer {
 	private int cid;
 	private String name;
 	private String city;
+	
+	//Default Constructors
 	public Customer() {
 		this.cid=101;
 		this.name="Bannu";
 		this.city="Chennai";
 	}
+	
+	//Parametarized Constructor
+	public Customer(int cid, String name, String city) {
+		this.cid=cid;
+		this.name=name;
+		this.city=city;
+	}
+	
+    //set() get()
 	public int getCid() {
 		return cid;
 	}
@@ -27,6 +38,8 @@ public class Customer {
 	public void setCity(String city) {
 		this.city = city;
 	}
+	
+	//toString()
 	@Override
 	public String toString() {
 		return "Customer2 [cid=" + cid + ", name=" + name + ", city=" + city + "]";
--- C2TC_CoreJavaProgram/src/Day2/Constructor/CustomerDemo.java
+++ C2TC_CoreJavaProgram/src/Day2/Constructor/CustomerDemo.java
@@ -3,17 +3,9 @@ package Day2.Constructor;
 public class CustomerDemo {
 	public static void main(String[] args) {
 		Customer c1= new Customer();
-		c1.setCid(101);
-		c1.setName("Harshii");
-		c1.setCity("Puducherry");
 		System.out.println(c1);
-	
-		
-		Customer c2= new Customer();
-		c2.setCid(102);
-		c2.setName("Bannu");
-		c2.setCity("Chennai");
+
+		Customer c2=new Customer(102,"Harshii","Pondy");
 		System.out.println(c2);
-		
 	}
 }
