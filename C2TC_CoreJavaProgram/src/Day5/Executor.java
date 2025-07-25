34bddc4350e81a2a455650b651eb283964e89a1e
harshithakrb <harshitha.krb@gmail.com>
2025-07-25 11:13:37 +0530

    This project contains all core java programs.

--- C2TC_CoreJavaProgram/src/Day5/Executor.java
+++ C2TC_CoreJavaProgram/src/Day5/Executor.java
@@ -5,6 +5,9 @@ public class Executor {
 	public static void main(String[] args) {
 		Trainer t1=new Trainer(102,"Harshi","Puducherry","JFS","Lead","C67");
 		System.out.println(t1);
+		
+		Student s1=new Student(101,"Balaji","Pondicherry",1,"JFS");
+		System.out.println(s1);
 
 	}
 
--- C2TC_CoreJavaProgram/src/Day5/Student.java
+++ C2TC_CoreJavaProgram/src/Day5/Student.java
@@ -1,5 +1,34 @@
 package Day5;
 
-public class Student {
-
+public class Student extends Person{
+	private int sid;
+	private String coursename;
+	
+	//Constructor from Person
+	public Student(int pid, String name, String city, int sid, String coursename) {
+		super(pid, name, city);
+		this.sid = sid;
+		this.coursename = coursename;
+	}
+    //getter and setter
+	public int getSid() {
+		return sid;
+	}
+	public void setSid(int sid) {
+		this.sid = sid;
+	}
+	public String getCoursename() {
+		return coursename;
+	}
+	public void setCoursename(String coursename) {
+		this.coursename = coursename;
+	}
+	
+	@Override
+	public String toString() {
+		return "Student [Person id:"+super.getPid()
+		+" Student Name:"+super.getName()
+		+" student id=" +sid 
+		+", Course Name=" + coursename +"]";
+	}
 }
