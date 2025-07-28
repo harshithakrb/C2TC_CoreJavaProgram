c01c84f9002b557967ddfc975815dbded9578f3d
harshithakrb <harshitha.krb@gmail.com>
2025-07-28 21:18:58 +0530

    This project contains all core java programs.

---
+++ C2TC_CoreJavaProgram/src/Dayseven/Overriding/HDFC.java
@@ -0,0 +1,11 @@
+//Program to demonstrate method overriding - Runtime Polymorphism
+package Dayseven.Overriding;
+
+//subclass
+public class HDFC extends RBI {
+	@Override
+	public float getRateOfInterest() {
+		return 6.8f;
+	}
+
+}
---
+++ C2TC_CoreJavaProgram/src/Dayseven/Overriding/RBI.java
@@ -0,0 +1,10 @@
+//Program to demonstrate method overriding - Runtime Polymorphism
+package Dayseven.Overriding;
+
+//Superclass
+public class RBI {
+	public float getRateOfInterest() {
+		return 6.7f;
+	}
+
+}
