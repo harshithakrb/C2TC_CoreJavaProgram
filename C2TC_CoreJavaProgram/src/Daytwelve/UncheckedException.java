501c09978b80e508b7b816791320026962b583e0
harshithakrb <harshitha.krb@gmail.com>
2025-08-01 14:10:23 +0530

    This project contains all core java programs.

---
+++ C2TC_CoreJavaProgram/src/Daytwelve/UncheckedException.java
@@ -0,0 +1,17 @@
+//Program to demonstrate unchecked exception
+package Daytwelve;
+
+public class UncheckedException {
+
+	public static void main(String[] args) {
+		int intArray[];
+		//try {
+			intArray = new int[] { 1, 2, 3, 4 };
+			System.out.println(intArray[6]);
+		//} catch (ArrayIndexOutOfBoundsException e) {
+			//System.err.println("Specified index does not exist. "+e.getMessage());
+		//}
+
+	}
+
+}
