cf53009b7cd6459a584ae48ae81ad6e9a1dc7b4c
harshithakrb <harshitha.krb@gmail.com>
2025-08-03 20:41:15 +0530

    This project contains all core java programs.

---
+++ C2TC_CoreJavaProgram/src/Dayfourteen/synchronization/InsufficientBalanceException.java
@@ -0,0 +1,13 @@
+package Dayfourteen.synchronization;
+
+public class InsufficientBalanceException extends Exception {
+
+	public InsufficientBalanceException() {
+		super("Insufficient balance in your account");
+	}
+
+	public InsufficientBalanceException(String message) {
+		super(message);
+	}
+
+}
